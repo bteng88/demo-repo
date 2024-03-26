7caad342522c10e61f202b58a67eca493cff5639
Brandon Teng <tengb23@Brandon-Teng.local>
2024-03-25 23:22:41 -0700

    coordinator is a squirrel

---
+++ hello_tkinter.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+def beaver():
+    print('BEAVER')
+
+def beaver2(event: tk.Event):
+    print('beaver?')
+
+def fighter():
+    print('FIGHTERX')
+
+def n():
+    print('coordinator is a squirrel')
+
+root = tk.Tk()
+root.geometry('400x400')
+button = tk.Button(root, text='BEAVER', command=beaver)
+button.bind('<Enter>', beaver2)
+button.pack()
+button2 = tk.Button(root, text='FIGHTERX', command=fighter)
+button2.pack()
+button3 = tk.Button(root, text='Coordinator', command=n)
+button3.pack()
+root.title("Hello Fighter")
+label = tk.Label(root, text="label")
+label.pack()
+root.mainloop()
